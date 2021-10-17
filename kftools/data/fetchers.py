e3b4ba405fddb736c2f2adb6f0e91f1747b1bb9b
John Griffiths <j.davidgriffiths@gmail.com>
2021-10-17 00:26:15 -0400

    fixed fetchers

--- kftools/data/fetchers.py
+++ kftools/data/fetchers.py
@@ -3,211 +3,109 @@ import requests, zipfile,gdown
 from datetime import datetime
 #from eegnb import DATA_DIR
 
+from kftools import data
+
 # dictionary format : kftools -> type -> experiments -> site -> subjects
 # the nifti dictionaries include 3 dictionaries each: hbo nifti, hbr nifti, and events.tsv (in this order)
 
-kftools_files_dict = {
-    'snirf-mom': {
-        'kf-ft':{
-            'kcni':{
-                0:{'kcni_000_0909-1523_ft_mom.snirf': '11Ji3X72pyQRTMGRQ_9Mt8NS646P6oR4e', 'kcni_000_0917-1313_ft_mom.snirf':'11DEWnSOhEjv3GDRE7d_XKCFtI-7Q2KCc'}}}},
-    'snirf-hb': {
-            'kf-ft':{
-            'kcni': {0:{'kcni_000_0909-1523_ft_hbm.snirf':'11Lcv-dczig_SLGHcyl8hstE3IS5R8oXO', 'kcni_000_0917-1313_ft_hbm.snirf':'1IOB4DZFDIkWzj9AtxygV8mQE1Ee2mtHt'}}}},
-    'nifti': {
-           'kf-ft':{
-           'kcni':{ 0:{'hbo':{'kcni_000_0909-1523_ft_hbo.nii.gz': '16F2jq5QT-gzbOMyzUU15bD6Uqd3qbh43', 
-                             'kcni_000_0917-1313_ft_hbo.nii.gz': '16MM3BKeSTZUeYt9UuKLP43OwdQQ9X2KE'}},
-                       'hbr':{'kcni_000_0909-1523_ft_hbr.nii.gz':'16HYwInXlIWpqezHpASP68IxNk3Wme9nI', 
-                             'kcni_000_0917-1313_ft_hbr.nii.gz':'16MZ4jDVhC34RnWj4DRwsL4HC0pjKVAfF'}},
-                       'events':{'kcni_000_0909-1523_ft_events.tsv':'1DXUIQcbVJg3dc4WAiC7xStbweF9v_4V1',
-                               'kcni_000_0917-1313_ft_events.tsv':'1CDrWzSNVIPCRDvQ541T2QLTo0-KmltDY'}}}}
+
+def load_info():
+
+  pth = os.path.dirname(data.__file__)
+  f = os.path.join(pth,'info.txt')
+
+  lines = [l.split(' ') for l in open(f, 'r').readlines()]
+  lines = lines[1:] # ignore the column headers in the info file
+  colhdrs = ['fname', 'site', 'subid', 'task', 'sesid', 'datetime', 'filetype', 'dlcode']
+  rowvals = []
+  for l1,l2 in lines:
+    addthis = [l1] + l1.split('.')[0].split('_') + [l2.replace('\n', '')]
+    rowvals.append(addthis)
+  df = pd.DataFrame(rowvals, columns=colhdrs)
+
+  return df
 
 
 
-def fetch_dataset(data_dir=None,
-    file_type=None,
-    experiment=None,
-    site="kcni",
-    subjects="all",
-    download_method="gdown",
-    f_ids = [0]):
-    """
-    Return a long-form filenames list and a table saying what
-    subject and session, and run each entry corresponds to
-    Usage:
-            data_dir = '/my_folder'
-            file_type = 'snirf-mom'
-            experiment = 'kf-ft'
-            subjects = [1]
-            # sessions = 'all'
-            snirf-mom_kf-ft_fnames = fetch_dataset(data_dir=data_dir, file_type = 'snirf-mom', subjects='all', experiment='kf-ft',
-            site='kcni')
-            snirf-mom_rec_fnames = fetch_dataset(data_dir=data_dir, file_type = 'snirf-mom', subjects=[1], experiment='rec',
-            site='kcni')
-    """
-    # List of file types available
-    file_type_list = [
-        "snirf-mom",
-        "snirf-hb",
-        "nifti"
-    ]
 
-    # List of experiments available
-    experiments_list = [
-        "kf-ft",
-        "kf-fl",
-        "kf-bh",
-        "rec",
-        "reo",
-        "mmn",
-    ]
+def fetch_file(data_dir=None,site='snic',task='ft',subid='sub001',sesid='ses01',
+               filetype='kp-nii-evs', download_method='gdown'):
+ 
+  """
+  Pull selected data files. 
+
+  Usage:
+  ------
+
+  from kftools.data import fetch_file
+
+  fetch_file(filetype='kp-nii-evs', sesid='ses02')
+
+  outdir = '/external/rprshnas01/netdata_kcni/jglab/Data/kftools_data'
+  fetch_file(filetype='kp-snf-hb', sesid='ses01', data_dir=outdir)
+
+
+  """
+
+
+ 
+  if 'NoneType' in str(type(data_dir)): 
+    data_dir = os.path.expanduser('~/.kftools')
+    if not os.path.isdir(data_dir): os.makedirs(data_dir)
+
+  df_info = load_info()
   
-    # List of sub available
-    experiments_list = [
-        "kf-ft",
-        "kf-fl",
-        "kf-bh",
-        "rec",
-        "reo",
-        "mmn",
-    ]
+  qstr = ''
+  for k_it,k in enumerate(['site', 'task', 'subid', 'sesid', 'filetype']):
+    if 'NoneType' not in str(type(k)):
+      if k_it!=0: qstr += " & "
+      qstr += "%s=='%s'" %(k, eval(k))
+      assert eval(k) in df_info[k].values
 
-        
-
-    # If no non-default top-level data path specified, use default
-    if data_dir == None:
-        data_dir = os.getcwd() #?
-
-    # check parameter entries
-    if experiment not in experiments_list:
-        raise ValueError("experiment not in database")
-
-    if file_type not in file_type_list:
-        raise ValueError("file type not in database")
-
-    if subjects == "all":
-        subjects = [0,1,2,3,4,5,6,7,8]
-
-    # selecting the files to download #   
-    if file_type == 'nifti':
-      hbo_ids = []
-      hbr_ids = []
-      events_ids = []
-
-      hbo_names = []
-      hbr_names = []
-      events_names = []
-        
-      for subject in subjects:
-        for i in range(len(kftools_files_dict[file_type[experiment[site[subject['hbo']]]]])):
-          hbo_ids.append(kftools_files_dict[file_type[experiment[site[subject[list('hbo'.keys())]]]]])[i]
-          hbo_names.append(kftools_files_dict[file_type[experiment[site[subject[list('hbo'.values())]]]]])[i]      
-        
-          hbr_ids.append(kftools_files_dict[file_type[experiment[site[subject[list('hbr'.keys())]]]]])[i]
-          hbr_names.append(kftools_files_dict[file_type[experiment[site[subject[list('hbr'.values())]]]]])[i]
-        
-          events_ids.append(kftools_files_dict[file_type[experiment[site[subject[list('events'.keys())]]]]])[i]
-          events_names.append(kftools_files_dict[file_type[experiment[site[subject[list('events'.values())]]]]])[i]  
-    
-
-    else:
-        
-      files_id = []
-      files_name = []
-
-      for subject in subjects:
-        for i in range(len(kftools_files_dict[file_type[experiment[site[subject]]]])):
-          files_id.append(kftools_files_dict[file_type[experiment[site[list(subject.keys())]]]])[i]
-          files_name.append(kftools_files_dict[file_type[experiment[site[list(subject.values())]]]])[i]
-
-        
-    # check if data has been previously downloaded
-    download_it = False
-    exp_dir = os.path.join(data_dir, '/kftools_datasets')
-    if not os.path.isdir(exp_dir):
-        download_it = True
-
-    if download_it:
-        # check if data directory exits. If not, create it
-        if os.path.exists(data_dir) is not True:
-            os.makedirs(data_dir)
-
-        destination = os.path.join(data_dir, "/kftools_datasets") 
-
-        if download_method == "gdown":
-            if file_type == 'nifti':
-             for each_file in hbo_ids:
-                  URL = "https://drive.google.com/uc?id=" + each_file
-                  gdown.download(URL, destination, quiet=False)
-        
-             for each_file in hbr_ids:
-                  URL = "https://drive.google.com/uc?id=" + each_file
-                  gdown.download(URL, destination, quiet=False)
-        
-             for each_file in events_ids:
-                  URL = "https://drive.google.com/uc?id=" + each_file
-                  gdown.download(URL, destination, quiet=False)        
-        
-            else:
-             for each_file in files_id:
-                  URL = "https://drive.google.com/uc?id=" + each_file
-                  gdown.download(URL, destination, quiet=False)
-
-        elif download_method == "requests":
-
-            URL = "https://docs.google.com/uc?export=download"
-
-            session = requests.Session()
-            response = session.get(
-                URL, params={"id": gdrive_locs[experiment]}, stream=True #%????
-            )
-
-            # get the confirmation token to download large files
-            token = None
-            for key, value in response.cookies.items():
-                if key.startswith("download_warning"):
-                    token = value
-
-            if token:
-                params = {"id": id, "confirm": token}
-                response = session.get(URL, params=params, stream=True)
-
-            # save content to the zip-file  
-            CHUNK_SIZE = 32768
-            with open(destination, "wb") as f:
-                for chunk in response.iter_content(CHUNK_SIZE):
-                    if chunk:
-                        f.write(chunk)
-
-        ## unzip the file
-        #with zipfile.ZipFile(destination, "r") as zip_ref:
-        #    zip_ref.extractall(data_dir)
+  idxstoget = df_info.query(qstr).index.values
+  for idx in idxstoget:
+    dlcode,fname = df_info.loc[idx][['dlcode', 'fname']].values
+    fname = os.path.join(data_dir, fname)
+    pull_file(dlcode,fname,download_method)
 
 
-    # returning the file directories as a list:
-    if file_type == 'nifti':
-      hbo_dirs = []
-      hbr_dirs = []
-      events_dirs = []
 
-      for i in hbo_names:
-        file_dir = glob.glob(destination + i)[0]
-        hbo_dirs.append(file_dir)        
+def pull_file(dlcode,destination,download_method):
 
-      for i in hbr_names:
-        file_dir = glob.glob(destination + i)[0]
-        hbr_dirs.append(file_dir)        
+  dest_file = destination.split('/')[-1]
+  print('\n\nDownloading %s\n' %dest_file)
 
-      for i in events_names:
-        file_dir = glob.glob(destination + i)[0]
-        events_dirs.append(file_dir)        
+  if download_method == "gdown":
 
-    else:
-      f_dirs = []
-      for i in files_name:
-        file_dir = glob.glob(destination + i)[0]
-        f_dirs.append(file_dir)
-        
-      return f_dirs
+    url = "https://drive.google.com/uc?id=" + dlcode
+    gdown.download(url, destination, quiet=False)
+
+
+  elif download_method == "requests":
+
+    url = "https://docs.google.com/uc?export=download"
+
+    session = requests.Session()
+    response = session.get(
+    url, params={"id": dlcode}, stream=True)
+
+    # get the confirmation token to download large files
+    token = None
+    for key, value in response.cookies.items():
+      if key.startswith("download_warning"):
+        token = value
+
+    if token:
+      params = {"id": id, "confirm": token}
+      response = session.get(url, params=params, stream=True)
+
+    # save content to the zip-file  
+    CHUNK_SIZE = 32768
+    with open(destination, "wb") as f:
+      for chunk in response.iter_content(CHUNK_SIZE):
+        if chunk:
+          f.write(chunk)
+
+
+
 
