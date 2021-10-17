5ffccab66c60ea16f27b3e1ac65e41e224a9c88a
John Griffiths <JohnGriffiths@users.noreply.github.com>
2021-10-17 17:14:11 -0400

    Update eg01r__fetching_data.py

--- examples/eg01r__fetching_data.py
+++ examples/eg01r__fetching_data.py
@@ -1,18 +1,39 @@
+# -*- coding: utf-8 -*-
+r"""
+=================================
+Downloading KFTools Example Data
+=================================
 
+
+"""  # noqa:E501
 
 from kftools.data import fetch_file
 
 data_dir = '/external/rprshnas01/netdata_kcni/jglab/Data/kftools_data'
 
 
-# Nii Events file
+# sphinx_gallery_thumbnail_number = 1
+
+# %%
+# First we do a thing etc etc.
+
+
+# %%
+# Nii Events File
+# ---------------------------------------------------
+#
 fetch_file(data_dir=data_dir, filetype='kp-nii-evs')
 
+# %%
 # Nii HbO file
+# ---------------------------------------------------
 fetch_file(data_dir=data_dir, filetype='kp-nii-hbo')
 
+# %%
 # HB Moments file
+# ---------------------------------------------------
 fetch_file(data_dir=data_dir, filetype='kp-snf-hbm')
+
 
 
 
