33e8493f8129e2197a4f899ee48b5a01d9eab61f
JohnGriffiths <JohnGriffiths@users.noreply.github.com>
2021-10-20 07:46:10 +0000

    deploy: f43db22fefd5c1f1dc2f27b0ddcfc5801ba463b2

--- _downloads/52d6c382984b4719368dce128475d7c1/eg04r__glm_analysis.py
+++ _downloads/52d6c382984b4719368dce128475d7c1/eg04r__glm_analysis.py
@@ -1,72 +1,53 @@
 # -*- coding: utf-8 -*-
 r"""
 =================================
-GLM based analysis
+SNIRF HbM GLM Analysis
 =================================
 
-Blah blah
 
-blah
-blah
-blah
-
-
-*****************
-Thing
-*****************
-
-Thingywot
-==============
-
-Blah blah
-
-
-***********************
-Anotherthing
-***********************
-
-
-Whatevs
-=======================
-
-Stuff
-Stuff
-Stuff
-
-"""  # noqa:E501
-
-import numpy as np
-import pandas as pd
-import mne
+"""  
 
 # sphinx_gallery_thumbnail_number = 1
 
 # %%
-# First we do a thing etc etc.
+# Importage
+# --------------------------------------------------
 
-pd.DataFrame(np.random.normal(size=(16, 100))).plot(kind='hist')
+# KF Tools and related imports
+from kftools.data import fetch_file
+from kftools.snirf import snirf_task_ana,plot_glm_contrast_topo
 
-# %%
-#
-# .. warning:: athing a not etc
-#              blah blah
-thisvar  = ['hbo', 'hbr', 'hbo', 'hbr',
-            'hbo', 'hbr', 'hbo', 'hbr']
 
 # %%
-# More stuff etc blah
-# blah blah
-#montage = mne.channels.make_standard_montage('artinis-octamon')
-#raw.set_montage(montage)
+# Grab the data
+# --------------------------------------------------
 
-# View the position of optodes in 2D to confirm the positions are correct.
-print('thing')#           ``something here`` to ``'eg'``.
+data_dir='.'
+ 
+fetch_file(data_dir=data_dir, filetype='kp-snf-hbm',
+           site='snic', task='ft', subid='sub001', sesid='ses02')
 
-#
-# More stuff
-# More stuff
-#
-# .. note:: Things
 
-#f = ''
-#res = run_pipeline(f,subselect_with='notnan')
+# %%
+# Specify and run the GLM model
+# ---------------------------------------------------
+
+#f = 'snic_sub001_ft_ses02_0917-1313_kp-snf-hbm.snirf'
+f = 'snic_sub001_ft_ses01_0909-1523_kp-snf-hbm.snirf'
+
+res = snirf_task_ana(f)
+
+res.keys()
+
+
+# %%
+# Look at GLM results
+# ---------------------------------------------------
+
+#glmest = res['glm_est']
+#contrst = res['estmrg_LgtR']
+#con = contrst.data
+
+glmest = res['glm_est']
+con = res['contrast_LgtR'].data
+disp = plot_glm_contrast_topo(glmest, con,sphere='auto')
