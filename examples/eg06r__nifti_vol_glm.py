46f5ad8a6236ac0640825c3df136d11a80afc828
John Griffiths <j.davidgriffiths@gmail.com>
2021-11-05 21:59:17 -0400

    nifti vol analysis update

--- examples/eg06r__nifti_vol_glm.py
+++ examples/eg06r__nifti_vol_glm.py
@@ -16,6 +16,14 @@ Nifti volumes - GLM analysis
 from kftools.data import fetch_file
 from kftools.nifti import ft_glm_ana
 
+import nibabel as nib
+from nilearn.image import mean_img
+from nilearn.surface import load_surf_mesh,load_surf_data,vol_to_surf
+from nilearn import datasets
+
+from nilearn.plotting import plot_glass_brain,plot_stat_map,plot_surf_stat_map
+from matplotlib import pyplot as plt
+
 # %%
 # Grab the data
 # --------------------------------------------------
@@ -38,42 +46,59 @@ nii_hbr_f = 'pitch_sub010_ft_ses01_1017-1706_kp-nii-hbr.nii.gz'
 nii_ev_f = 'pitch_sub010_ft_ses01_1017-1706_kp-nii-evs.tsv'
 
 # %% 
-# HbO analyses
+# Analysis and viz
 # --------------------------------------------------    
 
 # %%
-# Fit model
+# Run GLM analysis
 res_hbo =  ft_glm_ana(nii_hbo_f, nii_ev_f, out_fstr = '')
 z_thrs,glm,evs,cntrsts,img = res_hbo
+z = z_thrs['rightft_minus_rest']
 
 # %%
 # Glass brain plots
-z = z_thrs['rightft_minus_leftft']
-disp = plot_glass_brain(z,colorbar=True, threshold=1)#,vmin=10,vmax=20);
-a.set_title('right > left');
-
-z = z_thrs['leftft_minus_rightft']
-disp = plot_glass_brain(z,colorbar=True, threshold=1)#,vmin=10,vmax=20);
-a.set_title('left > right');
-
-
-
-# %% 
-# HbR analyses
-# -------------------------------------------------
-
-res_hbr =  ft_glm_ana(nii_hbr_f, nii_ev_f, out_fstr = '')
-z_thrs,glm,evs,cntrsts,img = res_hbr
+disp = plot_glass_brain(z,colorbar=True,threshold=5,black_bg=True)
 
 # %%
-# Glass brain plots
-z = z_thrs['rightft_minus_leftft']
-disp = plot_glass_brain(z,colorbar=True, threshold=1)#,vmin=10,vmax=20);
-a.set_title('right > left');
+# Slice view stat image plots 
+disp = plot_stat_map(z,colorbar=True,threshold=5,cut_coords=[-20,-20,60])
 
-z = z_thrs['leftft_minus_rightft']
-disp = plot_glass_brain(z,colorbar=True, threshold=1)#,vmin=10,vmax=20);
-a.set_title('left > right');
+# %%
+# Project to surface 
 
+fs5 = datasets.fetch_surf_fsaverage()
+
+lh_dat = vol_to_surf(z,surf_mesh=fs5.pial_left)
+rh_dat = vol_to_surf(z,surf_mesh=fs5.pial_right)
+
+lhc = load_surf_data(fs5.sulc_left)
+rhc = load_surf_data(fs5.sulc_right)
+
+lhi_vtx,lhi_tri = load_surf_mesh(fs5.infl_left)
+rhi_vtx,rhi_tri = load_surf_mesh(fs5.infl_right)
+rhi_vtx_mod = rhi_vtx.copy()
+rhi_vtx_mod[:,0] += 90
+lrhi_vtx = np.concatenate([lhi_vtx, rhi_vtx_mod],axis=0)
+lrhi_tri = np.concatenate([lhi_tri, rhi_tri+lhi_vtx.shape[0]])
+lrh_dat = np.concatenate([lh_dat,rh_dat],axis=0)
+lrhc = np.concatenate([lhc,rhc],axis=0)
+lrhi_vtx_rot = np.zeros_like(lrhi_vtx)
+lrhi_vtx_rot[:,0] = -lrhi_vtx[:,1]
+lrhi_vtx_rot[:,1] = lrhi_vtx[:,0]
+lrhi_vtx_rot[:,2] = lrhi_vtx[:,2]
+
+fig, ax = plt.subplots(ncols=3,subplot_kw={'projection': '3d'},figsize=(8,3))#, f)
+
+a = ax[0]
+disp = plot_surf_stat_map([lrhi_vtx_rot,lrhi_tri],lrh_dat, hemi='right', view='dorsal',
+                          bg_map=lrhc,threshold=5,axes=a,colorbar=False)#
+
+a = ax[1]
+disp = plot_surf_stat_map([lhi_vtx,lhi_tri],lh_dat, hemi='left', view='lateral',
+                          bg_map=lhc,threshold=5,axes=a,colorbar=False)#
+
+a = ax[2]
+disp = plot_surf_stat_map([rhi_vtx,rhi_tri],rh_dat, hemi='right', view='lateral',
+                          bg_map=rhc,threshold=5,axes=a,colorbar=False)#
 
 
