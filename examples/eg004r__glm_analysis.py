a764002a56f1d824c4477354effebbadb2672b93
John Griffiths <j.davidgriffiths@gmail.com>
2021-11-07 22:57:48 -0500

    updated example files

---
+++ examples/eg004r__glm_analysis.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+r"""
+=================================
+SNIRF HbM GLM Analysis
+=================================
+
+
+"""  
+
+# sphinx_gallery_thumbnail_number = 1
+
+# %%
+# Importage
+# --------------------------------------------------
+
+# KF Tools and related imports
+from kftools.data import fetch_file
+from kftools.snirf import snirf_task_ana,kf_plot_glm_contrast_topo
+
+
+# %%
+# Grab the data
+# --------------------------------------------------
+
+data_dir='.'
+ 
+fetch_file(data_dir=data_dir, filetype='kp-snf-hbm',
+            site='pitch', task='ft', subid='sub010', sesid='ses01')
+
+# %%
+# Specify and run the GLM model
+# ---------------------------------------------------
+
+f = 'pitch_sub010_ft_ses01_1017-1706_kp-snf-hbm.snirf'
+
+res = snirf_task_ana(f)
+
+res.keys()
+
+
+# %%
+# Look at GLM results
+# ---------------------------------------------------
+
+# %% 
+# HbO, Left > Right contrast
+
+glmest = res['glm_est']
+con = res['contrast_LgtR'].data
+disp = kf_plot_glm_contrast_topo(glmest, con,
+                                 vmin=0,vmax=0.5,sig_thr=0.00001)
+
+# %% 
+# HbO, Right > Left contrast
+
+glmest = res['glm_est']
+con = res['contrast_RgtL'].data
+disp = kf_plot_glm_contrast_topo(glmest, con,
+                                 vmin=0,vmax=0.5,sig_thr=0.00001)
+
+
+# %% 
+# HbR, Left > Right contrast
+
+glmest = res['glm_est']
+con = res['contrast_LgtR'].data
+disp = kf_plot_glm_contrast_topo(glmest, con, chromo='hbr',
+                                 vmin=0,vmax=0.3,sig_thr=0.0001)
+
+# %% 
+# HbR, Right > Left contrast
+
+glmest = res['glm_est']
+con = res['contrast_RgtL'].data
+disp = kf_plot_glm_contrast_topo(glmest, con, chromo='hbr',
+                                 vmin=0,vmax=0.5,sig_thr=0.0001)
+
+
+
+
---
+++ examples/eg007r__nifti_vol_fc.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+r"""
+=================================================
+Nifti volumes - functional connectivity analyses
+=================================================
+"""
+
+# sphinx_gallery_thumbnail_number = 1
+
+# %%
+# Importage
+# --------------------------------------------------
+
+# Generic Imports
+import os,sys,glob,numpy as np,pandas as pd
+
+# KF Tools and related imports
+from kftools.data import fetch_file
+from kftools.nifti import fc_for_seeds
+
+import nibabel as nib
+from nilearn.image import mean_img
+from nilearn.surface import load_surf_mesh,load_surf_data,vol_to_surf
+from nilearn import datasets
+from nilearn.datasets import load_mni152_template
+
+from nilearn.plotting import plot_glass_brain,plot_stat_map,plot_surf_stat_map
+from matplotlib import pyplot as plt
+
+# %%
+# Grab the data
+# --------------------------------------------------
+
+data_dir='.'
+
+fetch_file(data_dir=data_dir, filetype='kp-nii-hbo',
+            site='snic', task='rec', subid='sub008', sesid='ses02')
+
+nii_hbo_f = 'snic_sub008_rec_ses02_0920-1711_kp-nii-hbo.nii.gz'
+
+### seed coords from (Eggebrecht et al., 2014) ###
+seed_coord_dict = {'vis':(-19.5, -102, -3), 
+                   'aud':(-67.5, -27, 12), 
+                   'mot':(-67.5, -12, 27), 
+                   'DAN':(-58.5, -69, -6), 
+                   'FPC':(-52.5, 24, 33), 
+                   'DMN':(-43.5, 21, 51)}
+seed_coord_ser = pd.Series(seed_coord_dict)
+
+fs5 = datasets.fetch_surf_fsaverage()
+
+# %% 
+# Analysis and viz
+# --------------------------------------------------    
+
+# %%
+# Run FC GLM analysis
+k = 'DMN'
+res = fc_for_seeds(nii_hbo_f,dothese=[k])#,clip_vols=[k],radius=10)
+z_maps,z_maps_masked = res
+
+# %% 
+# Plot
+img = z_maps[k]
+dat = img.get_fdata()
+dat_pos = dat.copy()
+dat_pos[dat_pos<0] = 0
+img_pos = nib.Nifti1Image(dat_pos,img.affine)
+dat_pos_lhp = vol_to_surf(img_pos,fs5.pial_left)
+
+disp = plot_stat_map(img_pos, threshold=4,vmax=10)
+disp = plot_glass_brain(img_pos,threshold=4, vmax=10);
+disp = plot_surf_stat_map(fs5.pial_left,dat_pos_lhp,bg_map=fs5.curv_left,threshold=4,vmax=10)
+
+
