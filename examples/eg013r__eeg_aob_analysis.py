5f1918152b39ffef6f6a633faae3b64bf0133398
John Griffiths <j.davidgriffiths@gmail.com>
2021-11-19 13:08:15 -0500

    added template auditory oddball analysis

---
+++ examples/eg013r__eeg_aob_analysis.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+r"""
+=================================
+SNIRF EEG MNE Task Analysis
+=================================
+
+
+"""  
+
+# sphinx_gallery_thumbnail_number = 1
+
+# %%
+# Importage
+# --------------------------------------------------
+
+# KF Tools and related imports
+from kftools.data import fetch_file
+from kftools.snirf import snirf_eeg_task_ana
+
+
+# %%
+# Grab the data
+# --------------------------------------------------
+
+data_dir='.'
+ 
+fetch_file(data_dir=data_dir, filetype='kp-snf-hbm',
+            site='pitch', task='ft', subid='sub010', sesid='ses01')
+
+# %%
+# Specify and run the GLM model
+# ---------------------------------------------------
+
+f = 'pitch_sub010_ft_ses01_1017-1706_kp-snf-hbm.snirf'
+
+res = snirf_eeg_task_ana(f)
+
+res.keys()
+
+
